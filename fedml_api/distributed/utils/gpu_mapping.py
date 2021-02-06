3b2ec172e38bf8f7d6f95720370f57bd51c20f54
Amrest Chinkamol <amrest.c@ku.th>
2021-02-06 18:54:38 +0700

    Add logging

--- fedml_api/distributed/utils/gpu_mapping.py
+++ fedml_api/distributed/utils/gpu_mapping.py
@@ -4,13 +4,16 @@ import socket
 import torch
 import yaml
 
+logger = logging.getLogger(__name__)
 
-def mapping_processes_to_gpu_device_from_yaml_file(process_id, worker_number, gpu_util_file, gpu_util_key):
+def mapping_processes_to_gpu_device_from_yaml_file(process_id, worker_number, gpu_util_file, gpu_util_key, verbose=True):
+    if verbose:
+        logger.setLevel(logging.DEBUG)
     if gpu_util_file == None:
         device = torch.device("cpu")
-        logging.info(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        logging.info(" ################## You do not indicate gpu_util_file, will use CPU training  #################")
-        logging.info(device)
+        logger.info(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        logger.info(" ################## You do not indicate gpu_util_file, will use CPU training  #################")
+        logger.info(device)
         # return gpu_util_map[process_id][1]
         return device
     else:
@@ -24,14 +27,21 @@ def mapping_processes_to_gpu_device_from_yaml_file(process_id, worker_number, gp
             i = 0
             for host, gpus_util_map_host in gpu_util.items():
                 for gpu_j, num_process_on_gpu in enumerate(gpus_util_map_host):
+                    logger.debug(f"Host: {host} Mapping: {gpus_util_map_host} #GPU: {gpu_j} #NUM: {num_process_on_gpu}")
                     for _ in range(num_process_on_gpu):
                         gpu_util_map[i] = (host, gpu_j)
                         i += 1
-            logging.info("Process %d running on host: %s,gethostname: %s, gpu: %d ..." % (
+            logger.info("Process %d running on host: %s,gethostname: %s, gpu: %d ..." % (
                 process_id, gpu_util_map[process_id][0], socket.gethostname(), gpu_util_map[process_id][1]))
-            assert i == worker_number
+            try:
+                assert i == worker_number
+            except AssertionError as e:
+                logger.critical(f"Index {i} mismatch with worker_number: {worker_number}")
+                logger.critical(f"Unable to map the gpu from mapping configuration. ABORTED.")
+                logger.critical(f"Possible MPI_COMM_SIZE mismatch.")
+                raise e
 
         device = torch.device("cuda:" + str(gpu_util_map[process_id][1]) if torch.cuda.is_available() else "cpu")
-        logging.info(device)
+        logger.info(device)
         # return gpu_util_map[process_id][1]
         return device
